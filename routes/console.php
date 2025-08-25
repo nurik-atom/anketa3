e03f064514a6f5823fc2139a3e014883f3363d80
nurik-atom <baliikhanov@gmail.com>
2025-08-25 17:48:22 +0500

    25.08.2025

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('import:candidate-one')->everyMinute();
+// Schedule::command('import:candidate-one')->everyMinute();
 Schedule::command('telescope:prune --hours=6')->hourly();
