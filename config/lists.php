3ec45c1dc5cb7fa623e1063c4a4a10160f9e9fff
nurik-atom <baliikhanov@gmail.com>
2025-10-02 22:55:52 +0500

    Revert "23.09.2025"
    This reverts commit 467da0f509488518085fd1b41facff003c2c8124.

--- config/lists.php
+++ config/lists.php
@@ -43,28 +43,5 @@ return [
     'driving_license' => [
         '1' => 'Да',
         '0' => 'Нет'
-    ],
-    
-    'activity_spheres' => [
-        'finance_banking' => 'Финансы и банки',
-        'it_technology' => 'IT и технологии',
-        'healthcare_medicine' => 'Здравоохранение и медицина',
-        'education_science' => 'Образование и наука',
-        'retail_commerce' => 'Розничная торговля и коммерция',
-        'manufacturing_industry' => 'Производство и промышленность',
-        'construction_real_estate' => 'Строительство и недвижимость',
-        'transportation_logistics' => 'Транспорт и логистика',
-        'energy_utilities' => 'Энергетика и коммунальные услуги',
-        'agriculture_food' => 'Сельское хозяйство и пищевая промышленность',
-        'media_entertainment' => 'СМИ и развлечения',
-        'tourism_hospitality' => 'Туризм и гостеприимство',
-        'consulting_services' => 'Консалтинг и услуги',
-        'government_public' => 'Государственный сектор',
-        'non_profit_social' => 'Некоммерческие и социальные организации',
-        'legal_services' => 'Юридические услуги',
-        'marketing_advertising' => 'Маркетинг и реклама',
-        'hr_recruitment' => 'HR и рекрутинг',
-        'sales_business' => 'Продажи и бизнес-развитие',
-        'other' => 'Другое'
     ]
 ]; 
