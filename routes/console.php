8e66d4ada1ffb42b13b73d095980d27933c0cafe
nurik-atom <baliikhanov@gmail.com>
2025-11-11 17:33:47 +0500

    крон почта каждый 10 минут

--- routes/console.php
+++ routes/console.php
@@ -13,8 +13,8 @@ Schedule::command('telescope:prune --hours=6')->hourly();
 Schedule::command('cleanup:temp-files')->hourly();
 Schedule::command('gallup:clean-history --days=7')->daily();
 
-// Send one email per minute from import_candidates
-// Schedule::command('candidates:notify --limit=1')
-//     ->everyMinute()
-//     ->withoutOverlapping()
-//     ->onOneServer();
+//Send one email per minute from import_candidates
+Schedule::command('candidates:notify --limit=1')
+    ->everyTenMinutes()
+    ->withoutOverlapping()
+    ->onOneServer();
